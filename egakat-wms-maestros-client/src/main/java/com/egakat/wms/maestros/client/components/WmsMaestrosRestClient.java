29dd21dd3526fee79c6d086f78e17901960aa237
github-ek <esb.ega.kat@gmail.com>
2018-11-14 08:55:14 -0500

    Update egakat-wms-maestros-client/src/main/java/com/egakat/wms/maestros/client/components/WmsMaestrosRestClient.java

--- egakat-wms-maestros-client/src/main/java/com/egakat/wms/maestros/client/components/WmsMaestrosRestClient.java
+++ egakat-wms-maestros-client/src/main/java/com/egakat/wms/maestros/client/components/WmsMaestrosRestClient.java
@@ -1,10 +1,18 @@
 package com.egakat.wms.maestros.client.components;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.egakat.core.web.client.components.AbstractRestClientImpl;
 
 @Component
 public class WmsMaestrosRestClient extends AbstractRestClientImpl {
+	@Autowired
+	private RestTemplate restTemplate;
 
+	@Override
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
 }
